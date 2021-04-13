fe126f9b588c30fd509efea4ed811f7138850d20
Richard <richardyjkim@gmail.com>
2021-04-13 16:26:29 -0700

    post added

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./Develop/public'));
 
+// app.get
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  loadPage(res);
 
 });
 
@@ -23,8 +24,29 @@ app.get('/api/notes', (req, res) => {
   res.json(note);
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+})
+
+// app.post
+app.post('/api/notes', (req, res) => {
+  let newNote = req.body;
+  let notePath = path.join(__dirname, './Develop/db/db.json');
+  note.push(newNote);
+  fs.writeFile(notePath, JSON.stringify(note), err => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log("Note successfully saved");
+  })
+  res.json(newNote);
+  loadPage(res);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
 });
 
+loadPage = (res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+}
