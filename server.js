77e3b8536ea961c91d91c36e54fcc5fd98b08733
Richard <richardyjkim@gmail.com>
2021-04-13 15:17:58 -0700

    get req,res

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
-const fs = require('fs)');
+const fs = require('fs');
+const note = require('./Develop/db/db.json');
 
-app.use(express.urlencoded)({extended: true});
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./develop/public'))
+app.use(express.static('./Develop/public'));
 
-app.listen(PORT, ()=> {
-  console.log(`API server now on port ${PORT}`)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(note);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 });
 
